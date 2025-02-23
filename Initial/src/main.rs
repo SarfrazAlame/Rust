306e5f649a4aab20ab078afb86831d2ba7318760
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-02-23 09:19:15 +0530

    enum

--- Initial/src/main.rs
+++ Initial/src/main.rs
@@ -21,17 +21,143 @@
 //     return a%2==0;
 // }
 
+// ////////////////////////////////////////////////////// String
+// fn main(){
+//     let name = String::from("Sarfaraz");
+//     let len:usize =  get_len(name);
+//     println!("{}", len);
+
+// }
+
+// fn get_len(s:String)->usize{
+//     return s.len();
+// }
+
+// ////////////////////////////////////////////////////// borrowing
+// fn main() {
+//     let s1 = String::from("Sarfrazz");
+//     let len = get_len(&s1);
+
+//     print!("{}", len);
+
+//     print!("{}", &s1);
+// }
+
+// fn get_len(s2: &String) -> usize {
+//     return s2.len();
+// }
+
+// fn main() {
+//     let str = String::from("Sarfraz");
+
+//     let ref1 = &str;
+//     let ref2 = &str;
+//     let ref3 = &str;
+
+//     println!("{} {} {}", ref1, ref2, ref3)
+// }
+
+// ////////////////////////////////////////////////////// struct
+
+// struct Rect {
+//     height:f32,
+//     width:f32
+// }
+
+// fn main(){
+//     let r = Rect{
+//         width:10.0,
+//         height:10.0
+//     };
+
+//     println!("{} {}", r.width, r.height)
+// }
+
+// struct Rect {
+//     height: f32,
+//     width: f32,
+// }
+
+// impl Rect {
+//     fn area(&self)->f32{
+//         return self.height * self.width;
+//     }
+
+//     fn perimeter(&self)->f32{
+//         return  2.0 * (self.height + self.width);
+//     }
+
+//     fn print_something(a:u32){
+//         println!("Static function")
+//     }
+// }
+
+// fn main() {
+//     let r = Rect {
+//         width: 10.0,
+//         height: 10.0,
+//     };
+
+//     println!("{} {}", r.width, r.height);
+//     print!("{}", r.area());
+//     Rect::print_something(10);
+// }
+
+// ///////////////////////////////////////enum
+// enum Direction {
+//     North,
+//     South,
+//     West,
+//     East,
+// }
+
+// fn main() {
+//     let directoin = Direction::North;
+
+//     steer(directoin)
+// }
+
+// fn steer(dir: Direction) {
+//     match dir {
+//         Direction::East => print!("East Direction"),
+//         Direction::West => print!("West Direction"),
+//         _ => println!("Horizontal direction")
+//     }
+// }
+// use std::f32::consts::PI;
+
+// enum Shape {
+//     Sqare(f32),
+//     Cirlce(f32),
+//     Rectangle(f32, f32),
+// }
+
+// fn main() {
+//     let shape = Shape::Sqare(10.0);
+//     let shape_Circle = Shape::Cirlce(10.0);
+//     let shape_rect = Shape::Rectangle(10.0, 10.0);
+
+//     print!("{}", calculate_area(shape_Circle))
+// }
+
+// fn calculate_area(s: Shape) -> f32 {
+//     match s {
+//         Shape::Cirlce(radius) => PI * radius * radius,
+//         Shape::Sqare(side) => side * side,
+//         Shape::Rectangle(width, height) => width * height,
+//     }
+// }
 
 
-// ////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////////////// reading file
+
+use std::fs;
+
 fn main(){
-    let name = String::from("Sarfaraz");
-    let len:usize =  get_len(name);
-    println!("{}", len);
+    let contents = fs::read_to_string("a.txt");
 
+    match contents {
+        Ok(contents)=>println!("{}", contents),
+        Err(e)=>println!("Error while reading file")
+    }
 }
-
-fn get_len(s:String)->usize{
-    return s.len();
-}
-
