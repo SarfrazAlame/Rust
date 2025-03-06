f84493f9b3130dc4ef4a9b8f74655af0a25b0392
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-03-06 05:53:36 +0530

    practice

---
+++ practice/src/main.rs
@@ -0,0 +1,18 @@
+// // fn main() {
+// //     let x = 12;
+// //     let name = String::from("Sarfraz Alam");
+// //     println!("{} {}", x, name);
+// // }
+
+// fn main(){
+//     let name = String::from("Sarfraz Alam");
+//     let x = get_len(name);
+
+//     println!("{}", x)
+// }
+
+// fn get_len(str)->u32{
+//     return str.len();
+// }
+
+ 
---
+++ practice/target/debug/practice.d
@@ -0,0 +1,1 @@
+C:\Users\Sarfaraj\OneDrive\Desktop\Rust\practice\target\debug\practice.exe: C:\Users\Sarfaraj\OneDrive\Desktop\Rust\practice\src\main.rs
