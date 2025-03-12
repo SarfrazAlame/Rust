904ad8c0acfffec02d4100bf3f1bd00f7c513e18
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-03-12 05:46:07 +0530

    micros

---
+++ macros/src/restore.rs
@@ -0,0 +1,47 @@
+
+#[derive(Debug)]
+struct User {
+    username:String,
+    password:String,
+    age:u32
+}
+ 
+
+fn main(){
+    let u = User{
+        username:String::from("Sarfraz"),
+        password:String::from("Sarfraz"),
+        age:21
+    };
+
+    print!("{:?}", u); //debug
+    // print!("{}", u); // display
+}
+
+
+//////////////////////////////////////////////////manual
+
+use std::fmt::{write,Display};
+
+struct User {
+    username:String,
+    age:u32,
+}
+
+impl Display for User {
+    fn fmt(&self, f:&mut std::fmt::Formatter)->std::fmt::Result{
+        write!(f, "this is the user struct with age {}", self.age)
+    }
+}
+
+fn main(){
+    let u = User{
+        username:String::from("Sarfraz"),
+        age:21  
+    };
+
+    print!("{}", u.username);
+    print!("{}", u.age);
+
+    print!("{}", u)
+}
